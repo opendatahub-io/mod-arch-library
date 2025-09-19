ad579c865eb20076ab97dd6daaf857d49df843ee
Yulia Krimerman <juliapiterova@hotmail.com>
2025-09-19 14:02:14 -0400

    comments

--- mod-arch-shared/utilities/useBrowserUnloadBlocker.ts
+++ mod-arch-shared/utilities/useBrowserUnloadBlocker.ts
@@ -1,16 +1,11 @@
 import * as React from 'react';
 
-/**
- * Prevents accidental page unload when there are unsaved changes.
- * Adds a `beforeunload` listener while `shouldBlock` is true.
- */
 export const useBrowserUnloadBlocker = (shouldBlock: boolean): void => {
   React.useEffect(() => {
     if (!shouldBlock) return;
 
     const handleBeforeUnload = (event: BeforeUnloadEvent) => {
       event.preventDefault();
-      // Some browsers require returnValue to be set
       event.returnValue = '';
     };
 
