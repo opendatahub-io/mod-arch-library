ff631e36d737e7f2bfed2ed679b69970b9e1eb93
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2026-02-03 15:45:08 -0300

    feat: add namespace persistence to selector hook (#101)
    Signed-off-by: Guilherme Caponetto <638737+caponetto@users.noreply.github.com>

--- mod-arch-core/hooks/useNamespaceSelector.ts
+++ mod-arch-core/hooks/useNamespaceSelector.ts
@@ -1,22 +1,56 @@
 import { Namespace } from '~/types';
 import { useModularArchContext } from './useModularArchContext';
+import { useNamespacePersistence } from './useNamespacePersistence';
 
-export const useNamespaceSelector = (): {
+const DEFAULT_STORAGE_KEY = 'mod-arch.namespace.lastUsed';
+
+export type UseNamespaceSelectorArgs = {
+  storeLastNamespace?: boolean;
+  storageKey?: string;
+};
+
+export type UseNamespaceSelectorReturn = {
   namespacesLoaded: boolean;
   namespacesLoadError?: Error;
   namespaces: Namespace[];
   preferredNamespace: Namespace | undefined;
   updatePreferredNamespace: (namespace: Namespace | undefined) => void;
+  clearStoredNamespace: () => void;
   initializationError?: Error;
-} => {
+};
+
+export const useNamespaceSelector = (
+  args: UseNamespaceSelectorArgs = {},
+): UseNamespaceSelectorReturn => {
+  const { storeLastNamespace = false, storageKey = DEFAULT_STORAGE_KEY } = args;
   const context = useModularArchContext();
 
+  const {
+    namespacesLoaded,
+    namespacesLoadError,
+    namespaces,
+    preferredNamespace: contextPreferredNamespace,
+    updatePreferredNamespace: contextUpdatePreferredNamespace,
+    initializationError,
+  } = context;
+
+  const { preferredNamespace, updatePreferredNamespace, clearStoredNamespace } =
+    useNamespacePersistence({
+      namespaces,
+      contextPreferredNamespace,
+      contextUpdatePreferredNamespace,
+      storeLastNamespace,
+      storageKey,
+      namespacesLoaded,
+    });
+
   return {
-    namespacesLoaded: context.namespacesLoaded,
-    namespacesLoadError: context.namespacesLoadError,
-    namespaces: context.namespaces,
-    preferredNamespace: context.preferredNamespace,
-    updatePreferredNamespace: context.updatePreferredNamespace,
-    initializationError: context.initializationError,
+    namespacesLoaded,
+    namespacesLoadError,
+    namespaces,
+    preferredNamespace,
+    updatePreferredNamespace,
+    clearStoredNamespace,
+    initializationError,
   };
 };
